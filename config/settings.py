4034b0eab0b4918afa53d87a437bdd55aa1cd666
R0M4N1991 <futbolfreestyle1991@gmail.com>
2022-03-03 09:08:28 -0300

    Implementación cambio de contraseña

--- config/settings.py
+++ config/settings.py
@@ -143,3 +143,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
 AUTH_USER_MODEL = 'homepage.User'
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = 'easyrenting2021@gmail.com'
+EMAIL_HOST_PASSWORD = 'Easy1324.'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
--- core/homepage/urls.py
+++ core/homepage/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from core.homepage.views import *
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+from django.conf.urls import url
 
 
 urlpatterns = [
@@ -10,6 +12,11 @@ urlpatterns = [
     path('login', login, name="login"),
     path('register', register, name="register"),
     path('logout', logout),
+    path('reset/password_reset', PasswordResetView.as_view(template_name='departamento/registration/password_reset_forms.html', email_template_name="departamento/registration/password_reset_email.html"), name = 'password_reset'),
+    path('reset/password_reset_done', PasswordResetDoneView.as_view(template_name='departamento/registration/password_reset_done.html'), name = 'password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-za-z_\-]+)/(?P<token>.+)/$', PasswordResetConfirmView.as_view(template_name='departamento/registration/password_reset_confirms.html'), name = 'password_reset_confirm'),
+    path('reset/done',PasswordResetCompleteView.as_view(template_name='departamento/registration/password_reset_complete.html') , name = 'password_reset_complete'),
+    
 ]
 
 
